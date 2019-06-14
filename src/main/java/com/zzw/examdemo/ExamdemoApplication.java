16cde258523793b55af6015e74f9da49be202f66
UserZhongcoming <450834419@qq.com>
2019-06-14 20:29:57 +0800

    exam

--- src/main/java/com/zzw/examdemo/ExamdemoApplication.java
+++ src/main/java/com/zzw/examdemo/ExamdemoApplication.java
@@ -2,12 +2,28 @@ package com.zzw.examdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.doak.logbackPrjDemo.SpringBootApplicationStarter;
 
 @SpringBootApplication
-public class ExamdemoApplication {
+public class ExamdemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamdemoApplication.class, args);
+		System.out.println("ヾ(◍°∇°◍)ﾉﾞ    bootdo启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(ExamdemoApplication.class);
 	}
 
 }
